08b32ecd9e5aa15b4ce43f9e2bbb862e56f3ebb5
werewolfpy <72212785+werewolfpy@users.noreply.github.com>
2022-03-18 18:41:06 +0530

    pratice of binary search

---
+++ binarysearch.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int binarysearch(int arr[], int n, int key)
+{
+    int s = 0;int e = n - 1;
+    
+
+    while (s <= e)
+    {
+        int mid = (s + e)/2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if(arr[mid] > key)
+        {
+            e = mid-1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int n = sizeof(arr) / sizeof(int);
+    int key;
+    cin >> key;
+    int index = binarysearch(arr, n, key);
+    if (index == -1)
+    {
+        cout << "element doesnt exist";
+    }
+    else
+    {
+        cout << index;
+    }
+    return 0;
+}

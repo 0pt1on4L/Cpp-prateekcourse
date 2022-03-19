0750ceed50a13c15be46b4c7351c16c82789731e
werewolfpy <72212785+werewolfpy@users.noreply.github.com>
2022-03-19 23:49:30 +0530

    corrected my own version

--- countsort.cpp
+++ countsort.cpp
@@ -3,35 +3,26 @@
 #include <vector>
 
 using namespace std;
-void countsort(int a[],int n){
-	//Largest Element
-	int largest = -1;
+void countsort(int arr[],int n){
+    int largest = -1;
+    for(int i=0; i<n ; i++){
+        largest = max(largest, arr[i]);
+    }
 
-	for(int i=0;i<n;i++){
-		largest = max(largest,a[i]);
-	}
+    vector<int> freq(largest+1,0);
+    for(int i=0; i<n ; i++){
+        freq[arr[i]]++; 
+    }
 
-	//create a count array/vector
-	vector<int> freq(largest+1,0);
-
-	//Update the freq array 
-	for(int i=0;i<n;i++){
-		freq[a[i]]++;
-	}
-
-	//Put back the elements from freq into original array
-
-	int j = 0;
-	for(int i=0; i<=largest; i++){
-
-		while(freq[i]>0){
-			a[j] = i;
-			freq[i]--;
-			j++;
-		}
-	}
-	return;
-
+    int j =0; 
+    for(int i = 0; i<= largest;i++){
+        while(freq[i]>0){
+            arr[j] = i;
+            freq[i]--;
+            j++;
+        }
+    }
+    return;
 }
 
 int main(){

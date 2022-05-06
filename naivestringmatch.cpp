30ff60792dbf92c3e39427022a669299f564d8b3
WereWolfpy <72212785+WereWolfpy@users.noreply.github.com>
2022-05-06 13:36:13 +0530

    naive

---
+++ naivestringmatch.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <string.h>
+
+using namespace std;
+
+void search(char* pat,char* text){
+    int n= strlen(text);
+    int m = strlen(pat);  
+    for(int i=0;i<=n-m;i++){
+        int j;
+        for(j=0;j<m;j++)
+            if(text[i+j]!=pat[j])
+                break;  
+        if(j == m)
+            cout<<"Found at index"<<i<<endl;       
+    }
+
+}
+
+int main(){
+    char txt[] = "AABAACAADAABAAABAA";  
+    char pat[] = "AABA";
+    search(pat, txt);
+    return 0;
+}
